862b047c830bba71bf7c7e585f35bfb77301b429
JAE MELANEE BUGARIN (SHP-IRIS-ISD-OOCLL/MNL) <BUGARJA@OOCL.COM>
2019-10-25 08:30:06 +0800

    added sample file

---
+++ src/main/java/com/u/park/uparkbackend/SampleFile.java
@@ -0,0 +1,13 @@
+package com.u.park.uparkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UParkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UParkBackendApplication.class, args);
+	}
+
+}
