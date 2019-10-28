bc2e09a4a85eb7338aff93797e3df91eae78f695
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-28 12:36:53 +0800

    Added Location model, controller, service and repository

---
+++ src/main/java/com/u/park/uparkbackend/controller/LocationController.java
@@ -0,0 +1,32 @@
+package com.u.park.uparkbackend.controller;
+
+import com.u.park.uparkbackend.model.Location;
+import com.u.park.uparkbackend.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/api/locations")
+public class LocationController {
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Location>> getLocationByName(@RequestParam String name) {
+        return new ResponseEntity<>(locationService.getLocationByName(name), HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Location>> createLocations(@RequestBody List<Location> locationList) {
+        return new ResponseEntity<>(locationService.createLocations(locationList), HttpStatus.CREATED);
+    }
+}
